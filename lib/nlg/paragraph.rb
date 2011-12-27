df45959417cfa23b575b3b22c68f042d04ba7e25
Sricharan Sunder <sricharan92@gmail.com>
2011-12-27 14:51:56 +0530

    build_conjugations method added   + builds a hash of the conjugations related to an object recursively. SentenceObject class added   + has attributes which object has ( specifications, value, conjugated_with )   + has a new attribute conjugated which is a flag to convey if the object is conjugated(true) or represents the start of a sentence(false)

--- lib/nlg/paragraph.rb
+++ lib/nlg/paragraph.rb
@@ -13,16 +13,20 @@ module Nlg
       @sentences = []
       
       dataset['objects'].each do |object_type, object_details|
-        build_conjugations(object_type, dataset['objects'], nil) unless @objects.has_key?(object_type) 
+        unless @objects.has_key?(object_type)
+          sentence_object = SentenceObject.new(object_type, object_details, "conjugated" => false)
+          @objects[object_type] = sentence_object
+          build_conjugations(object_type, dataset['objects'], nil) 
+        end 
       end
     end
   
     def build
       puts @conjugations.inspect
       # Paragraph generation by generating individual sentences.   
-      @objects.each do |object_type, object_details|
-        sentence = Sentence.new(:subject => @subject, :specifications => @defaults.merge(object_details.specifications))
-        @sentences << sentence.build(object_type, object_details)
+      @objects.each do |object_type, sentence_object|
+        sentence = Sentence.new(:subject => @subject, :specifications => @defaults.merge(sentence_object.specifications))
+        @sentences << sentence.build(object_type, sentence_object)
       end
     end
 
@@ -34,12 +38,6 @@ module Nlg
       unless parent_object
         parent_object = object_type
       end
-      
-      # Creating the parent_object with "conjugated" => false
-      unless @objects.has_key?(object_type)
-        sentence_object = SentenceObject.new(object_type, object_details, "conjugated" => false)
-        @objects[object_type] = sentence_object
-      end 
      
       # Creating the @conjugations[parent_object] array recursively to finally
       # build the conjugations. 
@@ -49,17 +47,14 @@ module Nlg
             sentence_object = SentenceObject.new(conjugated_object, objects[conjugated_object], "conjugated" => true)
             @objects[conjugated_object] = sentence_object
           end
-          @conjugations[parent_object] = [] unless @conjugations[parent_object]
+          @conjugations[parent_object] ||= []
           unless @conjugations[parent_object].include?(conjugated_object) or @conjugations.has_key?(conjugated_object)
-            p "***********"
-            p parent_object
             @conjugations[parent_object] << conjugated_object
-            p conjugated_object
             build_conjugations(conjugated_object, objects, parent_object) # recursive call for building conjugations for a parent_object
           end
         end
       else
-        @conjugations[object_type] = [] if @objects[object_type].conjugated == false
+        @conjugations[object_type] = [] if @objects[object_type].conjugated == false # adding independent sentences which have no conjugations
       end
     
     end
